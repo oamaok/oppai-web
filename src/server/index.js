763e7d9f87727b2c15c69ae014dd07c36fece8f7
Teemu Pääkkönen <oamaok@gmail.com>
2019-06-26 10:25:03 +0300

    Fix server, styles

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const md5file = require('md5-file');
 const multiparty = require('connect-multiparty')();
 const oppai = require('./oppai');
 
@@ -15,32 +14,25 @@ app.post('/oppai', multiparty, (req, res) => {
   const file = req.files.map;
 
   if (file.name.substr(-4) !== '.osu' || file.size > 1024 * 1024 * 2) {
-    fs.unlink(file.path);
+    fs.unlinkSync(file.path);
     return res.send({ error: 'invalid or no file' }).end();
   }
 
-  const filename = path.resolve(__dirname, '../../beatmaps/', `${md5file(file.path)}.osu`);
 
-  return fs.readFile(file.path, (err, data) => {
-    fs.unlink(file.path);
-    if (err) return res.send({ error: err });
+  const mods = req.body.mods.split(',');
+  const acc = req.body.acc.replace(',', '.');
+  const options = Object.assign({}, req.body, { mods, acc });
 
-    return fs.writeFile(filename, data, (err) => {
-      if (err) return res.send({ error: err });
-
-      const mods = req.body.mods.split(',');
-      const acc = req.body.acc.replace(',', '.');
-      const options = Object.assign({}, req.body, { mods, acc });
-
-      oppai(filename, options)
-        .then((output) => {
-          res.send({ output });
-        })
-        .catch((error) => {
-          res.send({ error: 'error occured, pls no break' });
-        });
+  oppai(file.path, options)
+    .then((output) => {
+      res.send({ output });
+    })
+    .catch((error) => {
+      res.send({ error: 'error occured, pls no break' });
+    })
+    .finally(() => {
+      fs.unlinkSync(file.path);
     });
-  });
 });
 
 app.listen(3001);
